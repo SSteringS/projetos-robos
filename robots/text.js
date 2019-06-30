b34804b594d772df5536c4e68297b790a001d8a9
SSteringS <satyan.stering@gmail.com>
2019-06-30 14:33:35 -0300

    googleImages

--- robots/text.js
+++ robots/text.js
@@ -3,22 +3,26 @@ const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
 const sentenceBoundaryDetection = require('sbd')
 
 const watsonApiKey = require('../credentials/watson-nlu.json').apikey
-var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js')
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js')
 
-var nlu = new NaturalLanguageUnderstandingV1({
+const nlu = new NaturalLanguageUnderstandingV1({
     iam_apikey: watsonApiKey,
     version: '2018-04-05',
     url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
   });
 
+  const state = require('./state.js')
 
 async function robot(content) {
+    content = state.load()
+
     await fetchContentFromWikipedia(content)
     sanitizedContent(content)
     breakContentIntoSentences(content)
     limitMaximumSentences(content)
     await fetchKeywordOfAllSenteces(content)
-    console.log(content.sentences)
+
+    state.save(content)
 
     async function fetchContentFromWikipedia(content){
         
